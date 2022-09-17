9ce00a9b512a0b7abca0a9234745404d3ccdb334
Miroslav Janata <miroslav.janata@gmail.com>
2022-09-17 20:11:07 +0200

    dev

--- pvtest/js/Editor.js
+++ pvtest/js/Editor.js
@@ -2,12 +2,25 @@
 
     /* call on page startup - editor functionality initialize */
     initialize: function () {
+        let urlParams = new URLSearchParams(window.location.search);
+        if (urlParams.has('uri')) {
+            Editor.uri = decodeURI(urlParams.get('uri').replace(/["';><]/gi, '')); //avoid injection
 
+            // testing
+            Editor.start();
+        }
     },
 
     /* call on edit button click - check the login and switch to edit mode on success */
     start: function () {
-
+        // add edit icons to topics
+        let topicRows = $("table#details tr");
+        topicRows.each(function (index) {
+            r = $(this);
+            var attribute = $("td:nth-child(1)", r);
+            var content = $("td:nth-child(2) li", r);
+            content = content;
+        });
     },
 
     /* call on finish edit button click - switch to normal mode */
@@ -27,3 +40,7 @@
 
 
 };
+
+$(document).ready(function () {
+    Editor.initialize();
+});
