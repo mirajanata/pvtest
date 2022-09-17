c0bbc310f082fa187906c5a8c2198d70e7b1d1a4
Miroslav Janata <miroslav.janata@gmail.com>
2022-09-17 19:27:16 +0200

    Basic methods

--- pvtest/js/Editor.js
+++ pvtest/js/Editor.js
@@ -1,1 +1,29 @@
-﻿
+﻿var Editor = {
+
+    /* call on page startup - editor functionality initialize */
+    initialize: function () {
+
+    },
+
+    /* call on edit button click - check the login and switch to edit mode on success */
+    start: function () {
+
+    },
+
+    /* call on finish edit button click - switch to normal mode */
+    stop: function () {
+
+    },
+
+    /* call on attribute edit button click - open edit panel */
+    editAttribute: function (attribute) {
+
+    },
+
+    /* call on attribute save button click - POST to ticket database */
+    saveAttribute: function (attribute) {
+
+    }
+
+
+};
